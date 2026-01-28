d67d5449d72d064f626531032d675a0011d61cdf
Kol-0110 <kolalsisay61@gmail.com>
2026-01-28 05:51:02 -0200

    Create Reading file.cpp

---
+++ Reading file.cpp
@@ -0,0 +1,20 @@
+// READINA FILE  
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    fstream myFile;
+     myFile.open("FinalResults.txt", ios::in); //read
+    if ( myFile.is_open()) {
+        string line;
+        while (getline( myFile, line)) {
+            cout << line << endl;
+        }
+         myFile.close();
+        
+    } 
+    return 0;}
